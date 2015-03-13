f14206a6fb4fd59b0d17412b06ae43ebff89268e
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2015-03-13 12:11:43 +0100

    Evo i missing controllera :)

---
+++ Code/Nemesis.Web/Controllers/AssetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nemesis.Web.Controllers
+{
+    public class AssetController : Controller
+    {
+        // GET: Asset
+        public ActionResult AssetList()
+        {
+            return View();
+        }
+				public ActionResult AssetListHierarchical()
+				{
+					return View();
+				}
+				public ActionResult AssetTypes()
+				{
+					return View();
+				}
+				public ActionResult AssetAttributes()
+				{
+					return View();
+				}
+		}
+}

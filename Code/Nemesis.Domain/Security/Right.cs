d0f67fafb1dad9867958e3e525cb93694af39744
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2015-03-13 12:08:33 +0100

    dodana dva fajla koja su nedostajala

---
+++ Code/Nemesis.Domain/Security/Right.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemesis.Domain.Security
+{
+	public enum Right
+	{
+		UserAccountRead = 0,
+		UserAccountReadWrite = 1,
+
+		SecurityRoleRead = 10,
+		SecurityRoleReadWrite = 11,
+
+		TeamRead = 20,
+		TeamReadWrite = 21,
+
+		Accounting = 30,
+
+		NZIRead = 40,
+		NZIReadWrite = 41,
+
+		MaintenanceOrderRead = 50,
+		MaintenanceOrderReadWrite = 51,
+
+		ClientRead = 60,
+		ClientReadWrite = 61,
+
+		// prava vezana uz Objectives 
+		SummaryGoalRead = 1000,
+		SummaryGoalReadWrite = 1001,
+		SummaryGoalCreateObjectives = 1002,
+
+		QuarterObjectiveRead = 1010,
+		QuarterObjectiveReadWrite = 1011,
+		QuarterObjectiveReadWritePersonal = 1012,
+		QuarterObjectiveApprove = 1013,
+		QuarterObjectiveReadWriteTeamObjectives = 1014,
+
+		MonthObjectiveRead = 1020,
+		MonthObjectiveReadWrite = 1021,
+		MonthObjectiveReadWritePersonal = 1022,
+		MonthObjectiveApprove = 1023,
+		MonthObjectiveReadWriteTeamObjectives = 1024,
+		MonthObjectiveMove = 1025,
+
+		WeekObjectiveRead = 1030,
+		WeekObjectiveReadWrite = 1031,
+		WeekObjectiveReadWritePersonal = 1032,
+		WeekObjectiveApprove = 1033,
+		WeekObjectiveReadWriteTeamObjectives = 1034,
+		WeekObjectiveMove = 1035,
+
+		ObjectiveShowFromSubteams = 1040,
+
+		WorkActivityRead = 1100,
+		WorkActivityReadWrite = 1101,
+		WorkActivityReadWriteTeam = 1102,
+		WorkActivityGrade = 1105
+	}
+}
---
+++ Code/Nemesis.Domain/WorkActivityGrade.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemesis.Domain
+{
+	public enum WorkActivitiyGradeEnum
+	{
+		/// <summary>
+		/// Predstavlja nezadovoljavajuću ocjenu.
+		/// </summary>
+//		[LocalizedDescription("WorkActivitiesGradeUnsatisfactory")]
+		Unsatisfactory = 1,
+		/// <summary>
+		/// Predstavlja jedva zadovoljavajuću ocjenu.
+		/// </summary>
+		//[LocalizedDescription("WorkActivitiesGradeBarelySatisfactory")]
+		BarelySatisfactory = 2,
+		/// <summary>
+		/// Predstavlja zadovoljavajuću ocjenu.
+		/// </summary>
+		//[LocalizedDescription("WorkActivitiesGradeSatisfactory")]
+		Satisfactory = 3,
+		/// <summary>
+		/// Predstavlja vrlo dobru ocjenu.
+		/// </summary>
+		//[LocalizedDescription("WorkActivitiesGradeVeryGood")]
+		VeryGood = 4,
+		/// <summary>
+		/// Predstavlja izvrsnu ocjenu.
+		/// </summary>
+		//[LocalizedDescription("WorkActivitiesGradeExcellent")]
+		Excellent = 5
+	}
+	public class WorkActivityGrade
+	{
+        /// <summary>
+        /// Dohvaća ili postavlja identifikator ocjene dnevnih aktivnosti.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Dohvaća ili postavlja člana tima na kojeg se ocjena odnosi.
+        /// </summary>
+        public TeamMember TeamMember { get; set; }
+        /// <summary>
+        /// Dohvaća ili postavlja tim kojem pripada član koji se ocjenjuje.
+        /// </summary>
+        public Team Team { get; set; }
+        /// <summary>
+        /// Dohvaća ili postavlja datum ocjene dnevnih aktivnosti.
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Dohvaća ili postavlja ocjenu dnevnih aktivnosti.
+        /// </summary>
+				public WorkActivitiyGradeEnum Grade { get; set; }
+        /// <summary>
+        /// Dohvaća ili postavlja podatak jesu li dnevne aktivnosti
+        /// ažurirane nakon ocjenjivanja.
+        /// </summary>
+        public bool IsChangedAfterGrade { get; set; }
+
+        /// <summary>
+        /// Instancira novi <b>GradeWorkActivities</b> objekt.
+        /// </summary>
+				public WorkActivityGrade()
+        {
+            Id = -1;
+            TeamMember = null;
+            Team = null;
+            Date = DateTime.MinValue;
+            Grade = WorkActivitiyGradeEnum.Excellent;
+            IsChangedAfterGrade = false;
+        }
+	}
+}

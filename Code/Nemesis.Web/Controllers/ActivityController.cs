2f9fcf2f70ef28845ca017ce24d71ec31498a8c3
Antun <antun.flas@gmail.com>
2015-03-17 04:01:25 +0100

    Activities - osnovno (show, create) - controller

--- Code/Nemesis.Web/Controllers/ActivityController.cs
+++ Code/Nemesis.Web/Controllers/ActivityController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Nemesis.Domain;
+using Nemesis.DAL;
+using Nemesis.Web.Models;
+using Nemesis.Services;
+
 namespace Nemesis.Web.Controllers
 {
     public class ActivityController : Controller
@@ -11,7 +16,62 @@ namespace Nemesis.Web.Controllers
         // GET: Activity
         public ActionResult Index()
         {
-            return View();
+            using (var repo = new ActivityRepository(new NemesisContext()))
+            {
+                return View(
+                    ServiceProvider.ActiviyService(repo).All().Select(
+                        item => new ActivityViewModel(item)
+                    ).ToList()
+                );
+            }
+        }
+
+        public ActionResult Today()
+        {
+            using (var repo = new ActivityRepository(new NemesisContext()))
+            {
+                return View(
+                    "Index",
+                    ServiceProvider.ActiviyService(repo).TodayActivities().Select(
+                        item => new ActivityViewModel(item)
+                    ).ToList()
+                );
+            }
+        }
+
+        public ActionResult CurrentWeek()
+        {
+            using (var repo = new ActivityRepository(new NemesisContext()))
+            {
+                return View(
+                    "Index",
+                    ServiceProvider.ActiviyService(repo).CurrentWeekActivities().Select(
+                        item => new ActivityViewModel(item)
+                    ).ToList()
+                );
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View(new ActivityCreateModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(ActivityCreateModel model)
+        {
+            try
+            {
+                using (var repo = new ActivityRepository(new NemesisContext()))
+                {
+                    ServiceProvider.ActiviyService(repo).Create(model.CreateWorkActivity());
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }

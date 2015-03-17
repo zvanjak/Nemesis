5a98d0f484fa5d2d6226a074ce29849ad371b9b1
zvanjak <zvonimir.vanjak@gmail.com>
2015-03-17 15:28:18 +0100

    Promijenjen naziv contexta u IdentityModels.cs fajlu

--- Code/Nemesis.Web/Models/IdentityModels.cs
+++ Code/Nemesis.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Nemesis.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("NemesisContext", throwIfV1Schema: false)
         {
         }
 
